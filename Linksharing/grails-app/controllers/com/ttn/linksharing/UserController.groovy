5b54c87cbbcb06ec214c10f7852fa59e19d76f28
Prachi <prachijulka31@gmail.com>
2018-03-28 23:15:36 +0530

    Question3

--- Linksharing/grails-app/controllers/com/ttn/linksharing/UserController.groovy
+++ Linksharing/grails-app/controllers/com/ttn/linksharing/UserController.groovy
@@ -1,6 +1,8 @@
 package com.ttn.linksharing
-
+//Add User controller with index action that will render text 'user dahsboard'
 class UserController {
 
-    def index() { }
+    def index() {
+        render("user dashboard")
+    }
 }
